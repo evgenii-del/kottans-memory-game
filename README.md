77e36c7beb51219025635ad627210ea525b32a9d
Khasanov <zkhasanovv@gmail.com>
2020-11-07 23:22:02 +0200

    Added cards

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kottans memory game
--- js/main.js
+++ js/main.js
@@ -1,4 +1,67 @@
-const shuffle = (o) => {
-    for (let j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x) ;
-    return o;
-};
+const cards = [
+    {
+        id: 1,
+        img: 'images/1.jfif'
+    },
+    {
+        id: 2,
+        img: 'images/2.jfif'
+    },
+    {
+        id: 3,
+        img: 'images/3.jfif'
+    },
+    {
+        id: 4,
+        img: 'images/4.jfif'
+    },
+    {
+        id: 5,
+        img: 'images/5.jfif'
+    },
+    {
+        id: 6,
+        img: 'images/6.jfif'
+    },
+    {
+        id: 7,
+        img: 'images/7.jfif'
+    },
+    {
+        id: 8,
+        img: 'images/8.jfif'
+    }
+];
+
+const cards_container = document.querySelector('.js-cards_container');
+
+const renderItems = (cards) => {
+    let fragment = document.createDocumentFragment();
+    cards.forEach((card) => {
+        const block = document.createElement('div');
+        block.classList.add('flip-container');
+        block.innerHTML = `
+            <div class="flipper">
+                <div class="front">
+                    <img src="images/card-front.jfif" alt="card-front">
+                </div>
+                <div class="back">
+                    <img src="${card.img}" alt="${card.img}">
+                </div>
+            </div>
+        `;
+        fragment.appendChild(block);
+    })
+    cards_container.appendChild(fragment);
+}
+
+cards_container.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.value === 'flipper') {
+        target.style.transform = 'rotateY(180deg)';
+    }
+})
+
+const newArray = [...cards, ...cards].sort(() => 0.2 - Math.random());
+
+renderItems(newArray);
